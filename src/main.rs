c4df56a187c64511fa95321d14eb2903b49dd0d1
dickson <dickson.tung@qumonintelligence.com>
2020-02-13 16:32:03 +0800

    Fix deadlock

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,120 @@
-use std::{io::Error};
-use std::sync::{Arc, RwLock};
+use std::sync::Arc;
 
+use dashmap::mapref::one::Ref;
+use dashmap::DashMap;
+use futures::executor::block_on;
 use futures::stream::SplitSink;
-use log::{info, debug};
+use futures::{SinkExt, StreamExt};
+use log::{debug, info};
+use std::collections::HashSet;
+use std::vec::Vec;
 use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::WebSocketStream;
 use tungstenite::protocol::Message;
-use std::collections::{HashMap, HashSet};
-use futures::{StreamExt, SinkExt};
-use chashmap::CHashMap;
 use uuid::Uuid;
 
-type RoomMap = CHashMap<String, HashSet<String>>;
-
-type SessionMap = CHashMap<String, SplitSink<WebSocketStream<TcpStream>, Message>>;
-
-fn r_insert(map: Arc<RoomMap>, k: &str, v: &str) {
-    map.upsert(k.to_owned(),
-               || {
-                   let mut set = HashSet::new();
-                   set.insert(v.to_owned());
-                   set
-               },
-               |set|
-                   {
-                       if !set.contains(v) {
-                           set.insert(v.to_owned());
-                       }
-                   });
+struct ReferenceMaps {
+    // key: channel_id, val: [session_ids]
+    channel_map: DashMap<String, HashSet<String>>,
+    // key: session_id, val: [channel_ids]
+    session_map: DashMap<String, HashSet<String>>,
+    // key: session_id, val: [streams]
+    stream_map: StreamMap,
 }
 
-#[test]
-fn test_room_map() {
-    let r = Arc::new(RoomMap::new());
-    let size = 50;
-    for i in 0..size {
-        for j in 0..size {
-            let c = r.clone();
-            thread::spawn(move || {
-                r_insert(c, &i.to_string(), &j.to_string());
-            });
+impl ReferenceMaps {
+    pub fn new() -> ReferenceMaps {
+        ReferenceMaps {
+            channel_map: DashMap::new(),
+            session_map: DashMap::new(),
+            stream_map: DashMap::new(),
         }
     }
-    let ten_millis = time::Duration::from_millis(100);
-    thread::sleep(ten_millis);
-    for i in 0..size {
-        let c = r.clone();
-        let set = c.get(&i.to_string()).unwrap();
-        assert_eq!(set.len(), size);
+}
+
+type StreamMap = DashMap<String, SplitSink<WebSocketStream<TcpStream>, Message>>;
+
+fn bind_session_to_room(
+    refm: Arc<ReferenceMaps>,
+    session_id: &str,
+    channel_id: &str,
+    stream: SplitSink<WebSocketStream<TcpStream>, Message>,
+) {
+    let mut c_set = refm
+        .session_map
+        .entry(session_id.to_owned())
+        .or_insert(HashSet::new());
+    if !c_set.contains(channel_id) {
+        c_set.insert(channel_id.to_owned());
+    }
+    let mut s_set = refm
+        .channel_map
+        .entry(channel_id.to_owned())
+        .or_insert(HashSet::new());
+    if !s_set.contains(session_id) {
+        s_set.insert(session_id.to_owned());
+    }
+    if !refm.stream_map.contains_key(session_id) {
+        refm.stream_map.insert(session_id.to_owned(), stream);
     }
 }
 
-fn s_insert(map: Arc<SessionMap>, k: &str, v: SplitSink<WebSocketStream<TcpStream>, Message>) {
-    map.upsert(k.to_owned(),
-               || {
-                   v
-               },
-               |v|
-                   {});
+fn unbind(refm: Arc<ReferenceMaps>, session_id: &str) {
+    refm.stream_map.remove(session_id);
+    match refm.session_map.get(session_id) {
+        None => {}
+        Some(channel_set) => {
+            channel_set
+                .iter()
+                .for_each(|channel_id| match refm.channel_map.get_mut(channel_id) {
+                    None => {}
+                    Some(mut x) => {
+                        x.remove(session_id);
+                    }
+                });
+        }
+    };
+    refm.session_map.remove(session_id);
 }
 
-async fn send(room_map: Arc<RoomMap>, session_map: Arc<SessionMap>, room: &str, payload: &str) {
-    debug!("room_map: {:?}", room_map);
-    match room_map.get(room) {
-        None => debug!("no session set found for room: {}", room),
-        Some(sessions_set) => {
-            debug!("session_set: {:?}", sessions_set);
-            match sessions_set.iter().cloned().next() {
-                None => return,
-                Some(session_id) => {
-                    let mut w = session_map.get_mut(&session_id).unwrap();
-                    w.send(Message::Text(payload.to_string())).await;
-                }
+async fn send(refm: Arc<ReferenceMaps>, channel_id: &str, payload: &str) {
+    let session_set: Ref<String, HashSet<String>>;
+    match refm.channel_map.get(channel_id) {
+        None => {
+            debug!("no session set found for channel: {}", channel_id);
+            return;
+        }
+        Some(x) => {
+            session_set = x;
+        }
+    }
+    let mut error_session: Vec<String> = Vec::new();
+    session_set.iter().for_each(|session_id| {
+        let mut w = refm.stream_map.get_mut(session_id).unwrap();
+        match block_on(w.send(Message::Text(payload.to_string()))) {
+            Ok(_) => debug!("sent: {}", payload),
+            Err(err) => {
+                error_session.push(session_id.to_owned());
+                debug!("error send: {}", err)
             }
         }
+    });
+    drop(session_set);
+    for e in error_session {
+        unbind(refm.clone(), &e);
     }
-
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() {
     let mut settings = config::Config::default();
     settings
-        .merge(config::File::with_name("config/config.yaml")).unwrap()
-        .merge(config::Environment::with_prefix("SOCKET")).unwrap();
-
-    let room_map = Arc::new(RoomMap::new());
-    let session_map = Arc::new(SessionMap::new());
-
-    tokio::spawn(redis_sub(room_map.clone(), session_map.clone()));
+        .merge(config::File::with_name("config/config.yaml"))
+        .unwrap()
+        .merge(config::Environment::with_prefix("SOCKET"))
+        .unwrap();
+    let refm = Arc::new(ReferenceMaps::new());
+    tokio::spawn(redis_sub(refm.clone()));
     let addr = "0.0.0.0:".to_string() + &settings.get_str("socket.port").unwrap();
     let _ = env_logger::try_init();
     // Create the event loop and TCP listener we'll accept connections on.
@@ -96,12 +122,11 @@ async fn main() -> Result<(), Error> {
     let mut listener = try_socket.expect("Failed to bind");
     info!("Listening on: {}", addr);
     while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(accept_connection(stream, room_map.clone(), session_map.clone()));
+        tokio::spawn(accept_connection(stream, refm.clone()));
     }
-    Ok(())
 }
 
-async fn accept_connection(stream: TcpStream, room_map: Arc<RoomMap>, session_map: Arc<SessionMap>) {
+async fn accept_connection(stream: TcpStream, refm: Arc<ReferenceMaps>) {
     let addr = stream
         .peer_addr()
         .expect("connected streams should have a peer address");
@@ -110,28 +135,32 @@ async fn accept_connection(stream: TcpStream, room_map: Arc<RoomMap>, session_ma
     let ws_stream = tokio_tungstenite::accept_async(stream)
         .await
         .expect("Error during the websocket handshake occurred");
-
-    info!("New WebSocket connection: {}", addr);
-    let (write, _read) = ws_stream.split();
     let session_id = Uuid::new_v4();
-    s_insert(session_map, &session_id.to_string(), write);
-    r_insert(room_map, "5c886c69ca50950001362635.5e098bfaeaa38f0001d9dc84.message", &session_id.to_string());
+    info!(
+        "New WebSocket connection: {}, session_id: {}",
+        addr, session_id
+    );
+    let (write, _read) = ws_stream.split();
+    bind_session_to_room(
+        refm,
+        &session_id.to_string(),
+        "5c886c69ca50950001362635.5cb69d4d85c0790001b2fdff.message",
+        write,
+    )
 }
 
-async fn redis_sub(room_map: Arc<RoomMap>, session_map: Arc<SessionMap>) {
-    let client = redis::Client::open("redis://127.0.0.1:6379/")
-        .expect("Error opening redis client");
-    let mut con = client.get_connection()
+async fn redis_sub(refm: Arc<ReferenceMaps>) {
+    let client =
+        redis::Client::open("redis://127.0.0.1:6379/").expect("Error opening redis client");
+    let mut con = client
+        .get_connection()
         .expect("Error getting redis connection");
     let mut pubsub = con.as_pubsub();
-    pubsub.psubscribe("*")
-        .expect("Error creating pubsub");
+    pubsub.psubscribe("*").expect("Error creating pubsub");
     loop {
-        let msg = pubsub.get_message()
-            .expect("Error getting pubsub message");
-        let payload: String = msg.get_payload()
-            .expect("Error getting message payload");
-        send(room_map.clone(), session_map.clone(), msg.get_channel_name(), &payload).await;
+        let msg = pubsub.get_message().expect("Error getting pubsub message");
+        let payload: String = msg.get_payload().expect("Error getting message payload");
+        debug!("received: {}", payload);
+        send(refm.clone(), msg.get_channel_name(), &payload).await;
     }
 }
-
